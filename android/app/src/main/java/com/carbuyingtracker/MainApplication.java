cb4e85f521c9716bc79eab3ff37685461e342140
Dmitry-GH <dmitry.rudiuk@gmail.com>
2020-03-04 18:16:52 +0200

    init wix/react-native-navigation [Android]

--- android/app/src/main/java/com/carbuyingtracker/MainApplication.java
+++ android/app/src/main/java/com/carbuyingtracker/MainApplication.java
@@ -1,19 +1,18 @@
 package com.carbuyingtracker;
 
-import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new NavigationReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -42,7 +41,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
   }
 
